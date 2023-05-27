45c470f8ae990395602ef4b8af5feee4a9ca90c3
ricardo-mendonca <ricardo-mendonca@live.com>
2023-05-27 17:07:51 -0300

    Atualização da tela de login com consulta a api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,28 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    AppRoutingModule,
+
+    CommonModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
