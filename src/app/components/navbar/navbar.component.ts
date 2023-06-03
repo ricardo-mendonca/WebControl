e09a14690bda098143d7a5b03eddc3096a5a8ee3
RICARDO MENDONCA <ricardo-mendonca@users.noreply.github.com>
2023-06-02 21:50:33 -0300

    Atualização

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -3,14 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
   router: any;
 
-
-  logout(){
-   // Security.clear();
+  logout() {
+    // Security.clear();
     this.router.navigate(['/login']);
   }
-  }
+}
