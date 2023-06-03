24be7f93d471df84b88763097639b0ad1fb05ebb
RICARDO MENDONCA <ricardo-mendonca@users.noreply.github.com>
2023-06-02 21:44:52 -0300

    Atualização do sistama, criação das pastas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './pages/login/login.component';
+import { LoginComponent } from './auth/login/login.component';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { HttpClientModule } from '@angular/common/http';
+
 
 
 @NgModule({
--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
 
 
 @NgModule(
     {
         providers:[],
-        declarations:[DashboardComponent],
+        declarations:[
+          DashboardComponent,
+          NavbarComponent,
+        ],
         imports:[
             CommonModule,
             DashboardRoutingModule
